ed60613fe09d4666639af4425d14bc15b0dca398
Максим Григорьев <grigorev.maksim.1999@gmail.com>
2023-09-11 14:29:54 +0700

    step2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import './styles.scss';
 import 'bootstrap';
+import * as yup from 'yup';
 
-console.log("Hello World!");
+const input = document.querySelector('#url-input');
+const form = document.querySelector('.rss-form');
+
+const urls = [];
+
+const schema = yup.string().required().notOneOf(urls, 'alreadyLoaded').url('invalidUrl');
+
+const validateForm = async (url) => {
+  try {
+    await schema.validate(url);
+    input.classList.remove('is-invalid');
+    urls.push(url);
+  } catch (error) {
+    input.classList.add('is-invalid');
+  }
+};
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  await validateForm(input.value);
+  form.reset();
+  input.focus();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
-
 const config = {
     entry: './src/index.js',
     output: {
