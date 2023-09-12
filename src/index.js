9bfbf1846f22fa2156103c074078076fd4229531
Максим Григорьев <grigorev.maksim.1999@gmail.com>
2023-09-12 12:33:11 +0700

    step 2

--- src/index.js
+++ src/index.js
@@ -1,27 +1,56 @@
 import './styles.scss';
 import 'bootstrap';
 import * as yup from 'yup';
+import onChange from 'on-change';
+
+const state = {
+  validationState: 'valid',
+  urls: [],
+};
 
 const input = document.querySelector('#url-input');
 const form = document.querySelector('.rss-form');
 
-const urls = [];
+const schema = yup
+  .string()
+  .required('Введите адрес')
+  .notOneOf(state.urls, 'Адрес уже загружен')
+  .url('Некорректный адрес');
 
-const schema = yup.string().required().notOneOf(urls, 'alreadyLoaded').url('invalidUrl');
-
-const validateForm = async (url) => {
-  try {
-    await schema.validate(url);
-    input.classList.remove('is-invalid');
-    urls.push(url);
-  } catch (error) {
+const render = () => {
+  if (state.validationState === 'invalid') {
     input.classList.add('is-invalid');
+  } else {
+    input.classList.remove('is-invalid');
   }
 };
 
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  await validateForm(input.value);
-  form.reset();
-  input.focus();
+const watchedState = onChange(state, render);
+
+const validateForm = (url) => new Promise((resolve, reject) => {
+  schema
+    .validate(url)
+    .then(() => {
+      watchedState.validationState = 'valid';
+      watchedState.urls.push(url);
+      resolve();
+    })
+    .catch(() => {
+      watchedState.validationState = 'invalid';
+      reject();
+    });
 });
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  validateForm(input.value)
+    .then(() => {
+      form.reset();
+      input.focus();
+    })
+    .catch(() => {
+      console.log('Ошибка валидации');
+    });
+});
+
+export default watchedState;
